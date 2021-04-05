352ce787ec67bf5afcefcf3480980bebb146ab4d
Ivan Gabriele <ivangabriele@users.noreply.github.com>
2021-04-05 23:10:57 +0000

    feat: add latest-nodeXX tag (#4)

--- scripts/docker/install.sh
+++ scripts/docker/install.sh
@@ -6,7 +6,7 @@ apt-get update -qq > /dev/nul
 echo "⏳ Installing curl…"
 apt-get install -qq -y curl > /dev/nul
 echo "⏳ Installing Node.js…"
-curl -fsSL https://deb.nodesource.com/setup_${NODE_VERSION}.x | bash - > /dev/nul
+curl -fsSL "https://deb.nodesource.com/setup_${NODE_VERSION}.x" | bash - > /dev/nul
 apt-get install -qq -y nodejs > /dev/nul
 echo "⏳ Installing Yarn…"
 npm i -g yarn > /dev/nul
